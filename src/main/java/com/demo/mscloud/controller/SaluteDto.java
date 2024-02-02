1a89e4dc940933478147e26e468529c20b17ab3f
fabio <fabione2005@gmail.com>
2024-02-02 00:00:26 -0300

    fix de dependencia

--- src/main/java/com/demo/mscloud/controller/SaluteDto.java
+++ src/main/java/com/demo/mscloud/controller/SaluteDto.java
@@ -1,12 +1,35 @@
 package com.demo.mscloud.controller;
 
-import lombok.Data;
 
-@Data
+
 public class SaluteDto {
 
     private String nombre;
     private String apellido;
-
     private boolean saludo;
+
+    public boolean isSaludo() {
+        return saludo;
+    }
+
+    public void setSaludo(boolean saludo) {
+        this.saludo = saludo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
 }
