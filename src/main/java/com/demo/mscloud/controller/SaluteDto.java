28124d7514f09e40540d8f805f525631c914a4f2
fabio <fabione2005@gmail.com>
2024-02-01 23:45:02 -0300

    agregando endpoint post de prueba

---
+++ src/main/java/com/demo/mscloud/controller/SaluteDto.java
@@ -0,0 +1,12 @@
+package com.demo.mscloud.controller;
+
+import lombok.Data;
+
+@Data
+public class SaluteDto {
+
+    private String nombre;
+    private String apellido;
+
+    private boolean saludo;
+}
--- src/main/java/com/demo/mscloud/controller/UserController.java
+++ src/main/java/com/demo/mscloud/controller/UserController.java
@@ -3,6 +3,8 @@ package com.demo.mscloud.controller;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,6 +15,16 @@ public class UserController {
 	public ResponseEntity<String> dummy() {
 		System.out.println("hola render desde consola");
 		return ResponseEntity.ok("hola renderrr desde internet");
+	}
+
+	@PostMapping(value = "/saludo", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<String> addUser(@RequestBody SaluteDto request) {
+
+		String mensaje = request.getNombre()+" appelido: "+request.getApellido();
+
+		String response = request.isSaludo() ? "Hola "+mensaje : "Chao "+mensaje;
+
+		return ResponseEntity.ok(response);
 	}
 
 
